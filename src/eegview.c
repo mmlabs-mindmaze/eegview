4a3740f965756bb17b6fadf82c6eb826bbb34b27
Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
2012-01-22 21:46:35 +0100

    Update the copyright

--- src/eegview.c
+++ src/eegview.c
@@ -1,3 +1,20 @@
+/*
+    Copyright (C) 2009-2011  EPFL (Ecole Polytechnique Fédérale de Lausanne)
+    Laboratory CNBI (Chair in Non-Invasive Brain-Machine Interface)
+    Nicolas Bourdaud <nicolas.bourdaud@gmail.com>
+
+    This program is free software: you can redistribute it and/or modify
+    modify it under the terms of the version 3 of the GNU General Public
+    License as published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include <stdio.h>
 #include <mcpanel.h>
 #include <eegdev.h>
